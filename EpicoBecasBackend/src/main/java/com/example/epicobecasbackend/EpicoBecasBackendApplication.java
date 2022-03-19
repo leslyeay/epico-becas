cb1923aae509e6b435a0a9a9c01648da9264f448
David E. Barrera <dbarrerap@live.com>
2022-03-19 11:46:00 -0500

    Initial commit

---
+++ EpicoBecasBackend/src/main/java/com/example/epicobecasbackend/EpicoBecasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.epicobecasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicoBecasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicoBecasBackendApplication.class, args);
+    }
+
+}
